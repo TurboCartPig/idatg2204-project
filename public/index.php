351349793953f7dcb4ac5ce9baf5dcc69ded4d86
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-04-08 19:49:55 +0200

    Added all endpoints

--- public/index.php
+++ public/index.php
@@ -7,10 +7,54 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
+$app->get('/customer_rep/{employee_id}/orders', function (Request $request, Response $response, array $args) {
+
+});
+
+$app->get('customer_rep/{employee_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->put('customer_rep/{employee_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+
+});
+
+
+$app->get('customer_rep/{employee_id}/shipments',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->get('customers/{customer_id}/orders',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->post('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->delete('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->patch('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->get('customers/summary',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->get('transporters/shipments',function (Request $request, Response $response, array $args) {
+
+});
+
+$app->patch('transporters/shipments/{shipment_number}',function (Request $request, Response $response, array $args) {
+
+});
+
+
+$app->get('public/skis',function (Request $request, Response $response, array $args) {
+
 });
 
 $app->run();
