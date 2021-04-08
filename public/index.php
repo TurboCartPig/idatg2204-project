154f10daf7b9c27b4471e80b6b87a968390b33c8
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-04-08 19:52:04 +0200

    Added todo overview

--- public/index.php
+++ public/index.php
@@ -8,53 +8,51 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/customer_rep/{employee_id}/orders', function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->get('customer_rep/{employee_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->put('customer_rep/{employee_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
-
 $app->get('customer_rep/{employee_id}/shipments',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->get('customers/{customer_id}/orders',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->post('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->delete('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->patch('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->get('customers/summary',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->get('transporters/shipments',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->patch('transporters/shipments/{shipment_number}',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
-
 $app->get('public/skis',function (Request $request, Response $response, array $args) {
-
+    //TODO: Implement this endpoint
 });
 
 $app->run();
