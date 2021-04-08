2a043237f54102199892df3b7b43936cb7584919
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-04-08 19:57:13 +0200

    Formatting and new src folder

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -11,47 +12,47 @@ $app->get('/customer_rep/{employee_id}/orders', function (Request $request, Resp
     //TODO: Implement this endpoint
 });
 
-$app->get('customer_rep/{employee_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+$app->get('customer_rep/{employee_id}/orders/{order_number}', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->put('customer_rep/{employee_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+$app->put('customer_rep/{employee_id}/orders/{order_number}', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->get('customer_rep/{employee_id}/shipments',function (Request $request, Response $response, array $args) {
+$app->get('customer_rep/{employee_id}/shipments', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->get('customers/{customer_id}/orders',function (Request $request, Response $response, array $args) {
+$app->get('customers/{customer_id}/orders', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->post('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+$app->post('customers/{customer_id}/orders/{order_number}', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->delete('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+$app->delete('customers/{customer_id}/orders/{order_number}', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->patch('customers/{customer_id}/orders/{order_number}',function (Request $request, Response $response, array $args) {
+$app->patch('customers/{customer_id}/orders/{order_number}', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->get('customers/summary',function (Request $request, Response $response, array $args) {
+$app->get('customers/summary', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->get('transporters/shipments',function (Request $request, Response $response, array $args) {
+$app->get('transporters/shipments', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->patch('transporters/shipments/{shipment_number}',function (Request $request, Response $response, array $args) {
+$app->patch('transporters/shipments/{shipment_number}', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
-$app->get('public/skis',function (Request $request, Response $response, array $args) {
+$app->get('public/skis', function (Request $request, Response $response, array $args) {
     //TODO: Implement this endpoint
 });
 
