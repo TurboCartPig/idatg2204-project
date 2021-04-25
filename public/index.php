f615d4f4c6189835fe357754e71d9b7d5930b088
Sebastian Lindtvedt <lindtvedt.sebastian@gmail.com>
2021-04-25 15:23:33 +0200

    Moved endpoint 'update order state' to separate endpoint file

--- public/index.php
+++ public/index.php
@@ -40,40 +40,16 @@ $app->get('/customer_rep/{employee_id}/orders', function (Request $request, Resp
  */
 $app->put('/customer_rep/{employee_id}/orders/{order_number}', function (Request $request, Response $response, array $args) {
     $employeeID = $args['employee_id'];
-    $order_number = $args['order_number'];
+    $orderNumber = $args['order_number'];
 
     $db = $this->get('PDO');
     $dbInstance = $db->getDB();
 
-    $res = array();
-    $query = "SELECT * FROM orders 
-                INNER JOIN employee ON employee.number = orders.customer_rep
-                WHERE employee.number = :eid AND orders.order_number = :onb AND orders.order_state = 'Received'";
-    $stmt = $dbInstance->prepare($query);
-    $stmt->bindValue(":eid", $employeeID);
-    $stmt->bindValue(":onb", $order_number);
-    $stmt->execute();
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $res[] = $row;
-    }
+    $res = updateOrderState($dbInstance,$employeeID,$orderNumber);
 
-    // If there is not exactly ONE order matched by the above statement, then something is wrong.
-    if (count($res) != 1) {
-        $response->getBody()->write("The order is either not associated with this employee or it does not exits");
-        $response->withStatus(400);
-        return $response;
-    }
-
-    $query = "UPDATE orders 
-                SET orders.order_state = 'Open'
-                WHERE orders.order_number = :onb";
-    $stmt = $dbInstance->prepare($query);
-    $stmt->bindValue(":onb", $order_number);
-    $stmt->execute();
-
-    $response->withStatus(204);
+    $response->withStatus($res['status']);
+    $response->getBody()->write($res['body']);
     return $response;
-
 });
 
 /**
--- src/endpoints/customerRep.php
+++ src/endpoints/customerRep.php
@@ -19,3 +19,42 @@ function fetchOrders(PDO $dbInstance, mixed $employeeID): array
     }
     return $res;
 }
+
+
+
+function updateOrderState(PDO $dbInstance, mixed $employeeID, mixed $orderNumber): array
+{
+    $dbInstance->beginTransaction();
+    $res = array();
+    $query = "SELECT * FROM orders 
+                INNER JOIN employee ON employee.number = orders.customer_rep
+                WHERE employee.number = :eid AND orders.order_number = :onb AND orders.order_state = 'In production'";
+    $stmt = $dbInstance->prepare($query);
+    $stmt->bindValue(":eid", $employeeID);
+    $stmt->bindValue(":onb", $orderNumber);
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $res[] = $row;
+    }
+
+    // If there is not exactly ONE order matched by the above statement, then something is wrong.
+    if (count($res) != 1) {
+        $dbInstance->commit();
+        $data['body'] = "The order is either not associated with this employee or it does not exits";
+        $data['status'] = 400;
+        return $data;
+    }
+
+    $query = "UPDATE orders 
+                SET orders.order_state = 'Ready for shipping'
+                WHERE orders.order_number = :onb";
+    $stmt = $dbInstance->prepare($query);
+    $stmt->bindValue(":onb", $orderNumber);
+    $stmt->execute();
+
+    $dbInstance->commit();
+
+    $data['body'] = "Order state has been updated succesfully!";
+    $data['status'] = 204;
+    return $data;
+}
