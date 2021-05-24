17e515bc3418282c822f2f23ebfd892fab81cf47
Sebastian Lindtvedt <sebaskl@stud.ntnu.no>
2021-05-24 14:28:55 +0200

    improved tests for public endpoint

--- tests/unit/publicTest.php
+++ tests/unit/publicTest.php
@@ -32,5 +32,17 @@ class publicTest extends \Codeception\Test\Unit
         $res = getSkis($this->db->getDB());
 
         $this->assertCount(4, $res);
+
+        $first_ski = $res[0];
+
+        $this->assertEquals('skin',$first_ski['grip']);
+        $this->assertEquals('4900',$first_ski['msrp']);
+        $this->assertEquals('6',$first_ski['weight']);
+
+        $third_ski = $res[2];
+
+        $this->assertEquals('skin',$third_ski['grip']);
+        $this->assertEquals('4300',$third_ski['msrp']);
+        $this->assertEquals('5',$third_ski['weight']);
     }
 }
