e50f191a1c9d9ffda822ec9986e05ee3faf39436
Sebastian Lindtvedt <sebaskl@stud.ntnu.no>
2021-05-24 12:56:18 +0200

    test for order creation containing multiple skis added

--- tests/unit/customerTest.php
+++ tests/unit/customerTest.php
@@ -26,17 +26,40 @@ class CustomerTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testOrderCreation()
+    public function testOrderCreationSingleSki()
     {
-        $body['customer_rep'] = '2';
-        $body['customer_id'] = '3';
-
         $ski['ski_id'] = "3";
         $ski['quantity'] = "50";
+
+        $body['customer_rep'] = '2';
+        $body['customer_id'] = '3';
         $body['skis_in_order'][] = $ski;
+
+        $this->tester->dontSeeInDatabase('orders', array('total_price' => '215000', 'customer_rep' => '2', 'customer_id' => '3'));
 
         $res = createNewOrder($this->db->getDB(), $body);
 
         $this->tester->seeInDatabase('orders', array('total_price' => '215000', 'customer_rep' => '2', 'customer_id' => '3'));
+    }
+
+
+    public function testOrderCreationMultipleSkis()
+    {
+        $ski['ski_id'] = "3";
+        $ski['quantity'] = "50";
+
+        $ski_two['ski_id'] = "2";
+        $ski_two['quantity'] = "60";
+
+        $body['customer_rep'] = '2';
+        $body['customer_id'] = '3';
+        $body['skis_in_order'][] = $ski;
+        $body['skis_in_order'][] = $ski_two;
+
+        $this->tester->dontSeeInDatabase('orders', array('total_price' => '606800', 'customer_rep' => '2', 'customer_id' => '3'));
+
+        $res = createNewOrder($this->db->getDB(), $body);
+
+        $this->tester->seeInDatabase('orders', array('total_price' => '606800', 'customer_rep' => '2', 'customer_id' => '3'));
     }
 }
